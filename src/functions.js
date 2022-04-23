cb6f8fb7bcec4a132b40c22395e0a6c531a35de9
N0tAdm1n <n0tadm1n29@gmail.com>
2022-04-23 20:29:17 +0530

    Add get functions and return them

--- src/functions.js
+++ src/functions.js
@@ -2,3 +2,17 @@ export const clearCurrentTab = () => {
   const content = document.querySelector("#content");
   content.replaceChildren();
 };
+
+export const Food = (name, photo, price, description) => {
+  const _name = name;
+  const _photo = photo;
+  const _price = price;
+  const _description = description;
+
+  const getName = () => _name;
+  const getPhoto = () => _photo;
+  const getPrice = () => _price;
+  const getDescription = () => _description;
+
+  return { getName, getPhoto, getPrice, getDescription };
+};
